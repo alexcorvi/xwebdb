8808f9876503ee4a6d75a60cc57dd374f86e8215
Alex Corvi <ali.a.saleem@outlook.com>
2023-04-19 21:18:37 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { AvlTree, Node } from './core/avl';
-import { memoryStores } from './core/adapters/memory';
 import { Database } from "./database";
 import { BaseModel } from "./types/base-schema";
 import { Persistence, PersistenceEvent } from "./core";
@@ -8,13 +7,13 @@ import * as customUtils from "./core/customUtils";
 import { Datastore } from "./core";
 import {Index} from "./core"
 import * as modelling from "./core/model"
-import {kvAdapter, memoryAdapter} from "./core/adapters";
+import {kvAdapter} from "./core/adapters";
 
 const unifydb = {
     Database,
     BaseModel,
     adapters: {
-        kvAdapter, memoryAdapter, memoryStores
+        kvAdapter
     },
     Persistence,
     PersistenceEvent,
