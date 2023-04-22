1dc18933e68b574829c14ea15e791f235c2f1f0a
Alex Corvi <ali.a.saleem@outlook.com>
2023-04-22 15:10:47 +0300

    cleanup imports & exports

--- src/index.ts
+++ src/index.ts
@@ -10,25 +10,22 @@ import {
 	Index,
 	modelling,
 	adapters,
+	observable,
 } from "./core";
 import { Database } from "./database";
 import { BaseModel } from "./types/base-schema";
 
 const _internal = {
 	avl: { AvlTree, Node },
+	observable,
 	Cursor,
 	customUtils,
 	Datastore,
 	Index,
 	modelling,
 	Q,
-    Persistence,
-    PersistenceEvent
+	Persistence,
+	PersistenceEvent,
 };
 
-export {
-	Database,
-	BaseModel,
-	adapters,
-	_internal,
-};
+export { Database, BaseModel, adapters, _internal };
