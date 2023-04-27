deaa42dc2d5bb429a5fb726eac2c31da4925b224
Alex Corvi <ali.a.saleem@outlook.com>
2023-04-27 03:00:30 +0300

    return clean instance if passed undefined

--- src/types/base-schema.ts
+++ src/types/base-schema.ts
@@ -2,11 +2,11 @@ import * as customUtils from "../core/customUtils";
 import { RecursivePartial, NFGP, NFP } from "./common";
 
 class BaseModel {
-	static new<T extends object>(
-		this: new () => T,
-		data: RecursivePartial<NFGP<T>>
-	): T {
+	static new<T extends BaseModel>(this: new () => T, data?: RecursivePartial<NFGP<T>>): T {
 		const instance = new this();
+		if (typeof data !== "object" || data === null) {
+			return instance;
+		}
 		const keys = Object.keys({ ...instance, ...data });
 		for (let i = 0; i < keys.length; i++) {
 			const key = keys[i] as keyof T;
@@ -33,10 +33,7 @@ export class Doc extends BaseModel {
 	updatedAt?: Date;
 	createdAt?: Date;
 	// move strip defaults to BaseModel?
-	static stripDefaults<T extends object>(
-		this: new () => T,
-		existingData: T
-	): T {
+	static stripDefaults<T extends object>(this: new () => T, existingData: T): T {
 		const def = JSON.parse(JSON.stringify(new this()));
 		const keys = Object.keys(def);
 		const newData: any = {};
@@ -52,13 +49,19 @@ export class Doc extends BaseModel {
 export class SubDoc extends BaseModel {}
 
 function mapSubModel<T extends typeof SubDoc>(ctr: T, def: InstanceType<T>): InstanceType<T>;
-function mapSubModel<T extends typeof SubDoc>(ctr: T, def: Array<InstanceType<T>>): Array<InstanceType<T>>;
-function mapSubModel<T extends typeof SubDoc>(ctr: T, def: InstanceType<T> | Array<InstanceType<T>>): InstanceType<T> | Array<InstanceType<T>> {
-  return {
-	_$SHOULD_MAP$_: true,
-	def,
-	ctr,
-  } as any
+function mapSubModel<T extends typeof SubDoc>(
+	ctr: T,
+	def: Array<InstanceType<T>>
+): Array<InstanceType<T>>;
+function mapSubModel<T extends typeof SubDoc>(
+	ctr: T,
+	def: InstanceType<T> | Array<InstanceType<T>>
+): InstanceType<T> | Array<InstanceType<T>> {
+	return {
+		_$SHOULD_MAP$_: true,
+		def,
+		ctr,
+	} as any;
 }
 
 export { mapSubModel };
