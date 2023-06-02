d86c959cd131811d3abb1b2d32d66cb0d439418d
Alex Corvi <ali.a.saleem@outlook.com>
2023-06-02 23:29:56 +0300

    Update agg.ts

--- src/core/agg.ts
+++ src/core/agg.ts
@@ -1,78 +1,74 @@
 import * as model from "./model";
 import { SchemaKeyProjection, SchemaKeySort, Filter } from "../types/filter";
 
-class Aggregate<X> {
+export class Aggregate<X> {
 	subjects: X[] = [];
 	constructor(subjects: X[]) {
 		this.subjects = subjects;
 	}
-	$match<I = X>(filter: Filter<I>): Aggregate<I> {
-		this.subjects = this.subjects.filter((subject) => model.match(subject, filter));
-		return this as unknown as Aggregate<I>;
+
+	private removeUnusedID(arr: X[]): X[] {
+		return arr.map((item) => {
+			if ((item as any)._id === undefined) delete (item as any)._id;
+			return item;
+		});
 	}
-	$group<O, I = X>({ _id, reducer }: { _id: keyof I; reducer: (g: I[]) => O }): Aggregate<O> {
-		const groupsObj: Record<string, I[]> = {};
-		(this.subjects as unknown as I[]).forEach((subject) => {
+
+	$match(filter: Filter<X>): Aggregate<X> {
+		return new Aggregate(this.subjects.filter((subject) => model.match(subject, filter)));
+	}
+	$group<O>({ _id, reducer }: { _id: keyof X; reducer: (g: X[]) => O }): Aggregate<O> {
+		const groupsObj: Record<string, X[]> = {};
+		this.subjects.forEach((subject) => {
 			const propertyValue = JSON.stringify({ tmp: subject[_id] });
 			if (!groupsObj[propertyValue]) groupsObj[propertyValue] = [];
 			groupsObj[propertyValue].push(subject);
 		});
-		(this.subjects as unknown as O[]) = Object.values(groupsObj).map(reducer);
-		return this as unknown as Aggregate<O>;
+		return new Aggregate(Object.values(groupsObj).map(reducer));
 	}
-	$limit<I = X>(limit: number) {
-		this.subjects = this.subjects.slice(0, limit);
-		return this as unknown as Aggregate<I>;
+	$limit(limit: number) {
+		return new Aggregate(this.subjects.slice(0, limit));
 	}
-	$skip<I = X>(skip: number) {
-		this.subjects = this.subjects.slice(skip);
-		return this as unknown as Aggregate<I>;
+	$skip(skip: number) {
+		return new Aggregate(this.subjects.slice(skip));
 	}
-	$addFields<O, I = X>(adder: (this: I) => O) {
-		this.subjects = this.subjects.map((subject) => ({
-			...subject,
-			...adder.call(subject as unknown as I),
-		}));
-		return this as unknown as Aggregate<O & I>;
-	}
-	$sort<I = X>(sort: SchemaKeySort<I>) {
-		(this.subjects as unknown as I[]) = model.sort(this.subjects as unknown as I[], sort);
-		return this as unknown as Aggregate<I>;
-	}
-	$project<I = X>(project: SchemaKeyProjection<I>) {
-		(this.subjects as unknown as I[]) = model.project(
-			this.subjects as unknown as I[],
-			project
+	$addFields<O>(adder: (subject: X) => O) {
+		return new Aggregate(
+			this.subjects.map((subject) => ({
+				...subject,
+				...adder(subject),
+			}))
 		);
-		return this as unknown as Aggregate<I>;
+	}
+	$sort(sortCriteria: SchemaKeySort<X>) {
+		return new Aggregate(model.sort(this.subjects.slice(0), sortCriteria));
+	}
+	$project(project: SchemaKeyProjection<X>) {
+		return new Aggregate(this.removeUnusedID(model.project(this.subjects, project)));
 	}
 	$unwind<
 		F extends {
-			[Key in keyof I]: I[Key] extends any[] ? Key : never;
-		}[keyof I],
-		I = X,
-		E = I[F] extends (infer Elem)[] ? Elem : never
+			[Key in keyof X]: X[Key] extends any[] ? Key : never;
+		}[keyof X],
+		E = X[F] extends (infer Elem)[] ? Elem : never
 	>(fieldName: F) {
-		type unwound = I & { [Key in Exclude<keyof I, F>]: I[Key] } & {
+		type U = X & { [Key in Exclude<keyof X, F>]: X[Key] } & {
 			[Key in F]: E;
 		};
-		const unwoundSubjects: unwound[] = [];
+		const unwoundSubjects: U[] = [];
 		for (let index = 0; index < this.subjects.length; index++) {
-			const subject = this.subjects[index] as unknown as I;
-			const fieldArray = subject[fieldName] as I[F];
+			const subject = this.subjects[index];
+			const fieldArray = subject[fieldName];
 			if (Array.isArray(fieldArray)) {
 				for (const element of fieldArray) {
-					const unwoundSubject = { ...subject, [fieldName]: element } as I & {
-						[Key in Exclude<keyof I, F>]: I[Key];
-					} & { [Key in F]: E };
+					const unwoundSubject: U = { ...subject, [fieldName]: element };
 					unwoundSubjects.push(unwoundSubject);
 				}
 			} else {
-				unwoundSubjects.push(subject as unwound);
+				unwoundSubjects.push(subject as U);
 			}
 		}
-		(this.subjects as unknown as unwound[]) = unwoundSubjects;
-		return this as unknown as Aggregate<Omit<I, F> & { [Key in F]: E }>;
+		return new Aggregate(unwoundSubjects);
 	}
 	toArray(): X[] {
 		return this.subjects;
